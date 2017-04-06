9bb3fb4a4c968fc78a59d436485904990e6cfcec
Darren Crossley <darren@studiothirty.io>
2017-04-06 17:53:52 +0200

    added base_url update based on env var.

--- ojs-startup.sh
+++ ojs-startup.sh
@@ -4,13 +4,14 @@ sed -i -e "s/username = ojs/username = ${OJS_DB_USER}/g" /var/www/ojs/config.inc
 sed -i -e "s/password = ojs/password = ${OJS_DB_PASSWORD}/g" /var/www/ojs/config.inc.php
 sed -i -e "s/name = ojs/name = ${OJS_DB_NAME}/g" /var/www/ojs/config.inc.php
 
+sed -i -e "s#base_url = [\"]http://pkp.sfu.ca/ojs[\"]#base_url = \"${BASE_URL}\"#g" /var/www/ojs/config.inc.php
+
 sed -i -e "s/\/var\/www\/html/\/var\/www\/ojs/g" /etc/apache2/sites-available/000-default.conf
 sed -i -e "s/www.example.com/${SERVERNAME}/g" /etc/apache2/sites-available/000-default.conf
 sed -i -e "s/\/var\/log\/apache2/${APACHE_LOG_DIR}/g" /etc/apache2/sites-available/000-default.conf
 sed -i -e "s/error.log/%Y-%m-%d+${LOG_NAME}-error.log/g" /etc/apache2/sites-available/000-default.conf
 sed -i -e "s/access.log/%Y-%m-%d+${LOG_NAME}-error.log/g" /etc/apache2/sites-available/000-default.conf
 
-LOG_NAME
 # Start the cron service in the background.
 cron -f &
 echo "[OJS Startup] Started cron"
