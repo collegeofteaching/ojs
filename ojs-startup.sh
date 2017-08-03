5086cd696d09a3239ecc67c911e72c6a047f1282
liamseanbrady <liamseanbrady@gmail.com>
2017-08-03 16:04:53 +0100

    Fix sed to replace the whole line for DB name
    Previously, `sed` was replacing only a portion of the line
    containing the DB name. This resulted in a part of the
    DB name being left over each time the container was restarted,
    leaving a string like 'name = ojs-production-production-production'.
    
    Now, we are using sed to replace the contents of the whole line,
    which should fix a problem we're having with the container being
    able to find the MySQL DB.

--- ojs-startup.sh
+++ ojs-startup.sh
@@ -2,7 +2,7 @@
 sed -i -e "s/host = localhost/host = ${OJS_DB_HOST}/g" /var/www/ojs/config.inc.php
 sed -i -e "s/username = ojs/username = ${OJS_DB_USER}/g" /var/www/ojs/config.inc.php
 sed -i -e "s/password = ojs/password = ${OJS_DB_PASSWORD}/g" /var/www/ojs/config.inc.php
-sed -i -e "s/name = ojs/name = ${OJS_DB_NAME}/g" /var/www/ojs/config.inc.php
+sed -i -e "s/name = ojs.*/name = ${OJS_DB_NAME}/g" /var/www/ojs/config.inc.php
 
 sed -i -e "s#base_url = [\"]http://pkp.sfu.ca/ojs[\"]#base_url = \"${BASE_URL}\"#g" /var/www/ojs/config.inc.php
 sed -i -e "s/installed = Off/installed = ${OJS_INSTALLED}/g" /var/www/ojs/config.inc.php
